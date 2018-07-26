10bb1da7d8fe9601df70ca2f60f912ec63795d66
Weiyu Chen <weiyu.chen@shopee.com>
2018-07-26 18:36:22 +0800

    feat: init models

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/python
+
 """
 Django settings for my_blog project.
 
@@ -105,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -115,8 +118,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+PAGE_NUM = 3 # 每页显示3篇文章
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
